a2fe3953f84c3a39f51c30c1059cbfde992b939c
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-04-27 17:13:49 -0300

    feat: get unique industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
@@ -16,7 +17,12 @@ def get_unique_industries(path: str) -> List[str]:
     list
         List of unique industries
     """
-    raise NotImplementedError
+    data = read(path)
+    industries = set()
+    for job in data:
+        if job["industry"]:
+            industries.add(job["industry"])
+    return list(industries)
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
