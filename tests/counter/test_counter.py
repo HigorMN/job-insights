e7f718b57e1bb499a07599e491b2c3431b5071ef
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-04-27 19:07:21 -0300

    feat: test counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    file_path = "data/jobs.csv"
+    assert count_ocurrences(file_path, "PYTHON") == 1639
+    assert count_ocurrences(file_path, "JavaSCRIPT") == 122
+    assert count_ocurrences(file_path, "java") == 676
